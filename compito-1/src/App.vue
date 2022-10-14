d907d5c98be3411e024db488cbec6adaa2132b87
Francesco De Sìmini <francescod995@gmail.com>
2022-10-14 23:33:40 +0200

    header

--- compito-1/src/App.vue
+++ compito-1/src/App.vue
@@ -1,22 +1,44 @@
 <template>
   <div id="app">
-    <HeaderComp/>
+    <HeaderComp :userMail="userMail"/>
+    <MainComp/>
   </div>
 </template>
 
 <script>
 import HeaderComp from './components/HeaderComp.vue'
+import MainComp from './components/MainComp.vue'
 
 export default {
   name: 'App',
   components: {
-    HeaderComp
+    HeaderComp,
+    MainComp
+  },
+  data() {
+    return {
+      userMail: 'info@spedity.com',
+
+    }
   }
 }
 </script>
 
 <style lang="scss">
+@import '@/assets/styles/palette.scss';
+
   #app {
     font-family: Avenir, Helvetica, Arial, sans-serif;
+    font-size: 14px;
+  }
+
+  // Buttons
+  .btn {
+    font-size: 14px !important;
+  }
+
+  .btn-primary {
+        background-color: $blue !important;
+        color: white;
   }
 </style>
---
+++ compito-1/src/components/MainComp.vue
@@ -0,0 +1,23 @@
+<template>
+    <main>
+        
+    </main>
+</template>
+
+<script>
+export default {
+  name: 'MainComp',
+  data() {
+    return {
+
+    }
+  },
+  props: [],
+}
+</script>
+
+<style scoped>
+@import '@/assets/styles/palette.scss';
+
+
+</style>
