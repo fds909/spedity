8d67f23d691f413fb8a963f5863546530191e653
Francesco De Sìmini <francescod995@gmail.com>
2022-10-14 20:46:49 +0200

    init commit

---
+++ compito-1/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <HeaderComp/>
+  </div>
+</template>
+
+<script>
+import HeaderComp from './components/HeaderComp.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HeaderComp
+  }
+}
+</script>
+
+<style lang="scss">
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+  }
+</style>
